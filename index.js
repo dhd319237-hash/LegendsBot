2e864c49018e1dbd5ed024702252452a6a6694b9
dhd319237-hash <dhd319237@gmail.com>
2026-06-27 20:49:20 +0300

    move Bot2 to another voice channe1

--- index.js
+++ index.js
@@ -1,62 +1,34 @@
-require("dotenv").config();
-
-const { Client, GatewayIntentBits } = require("discord.js");
-const { joinVoiceChannel } = require("@discordjs/voice");
-
-// البوت الأول
-const client1 = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
-
-// البوت الثاني
-const client2 = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
-
 // تشغيل البوت الأول
 client1.once("ready", async () => {
   console.log(`✅ Bot 1: ${client1.user.tag}`);
 
-  try {
-    const guild = await client1.guilds.fetch("1516709826695004182");
-    const channel = await guild.channels.fetch("1518871777839943710");
+  const guild = await client1.guilds.fetch("1516709826695004182");
+  const channel = await guild.channels.fetch("1518871777839943710");
 
-    joinVoiceChannel({
-      channelId: channel.id,
-      guildId: guild.id,
-      adapterCreator: guild.voiceAdapterCreator,
-      selfDeaf: true,
-      selfMute: false,
-    });
-
-    console.log("🎧 Bot 1 joined voice channel!");
-  } catch (err) {
-    console.error(err);
-  }
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: true,
+    selfMute: false,
+  });
 });
 
 // تشغيل البوت الثاني
 client2.once("ready", async () => {
   console.log(`✅ Bot 2: ${client2.user.tag}`);
 
-  try {
-    const guild = await client2.guilds.fetch("1516709826695004182");
-    const channel = await guild.channels.fetch("1518871738753220649");
+  const guild = await client2.guilds.fetch("1516709826695004182");
+  const channel = await guild.channels.fetch("1518871857783111820");
 
-    joinVoiceChannel({
-      channelId: channel.id,
-      guildId: guild.id,
-      adapterCreator: guild.voiceAdapterCreator,
-      selfDeaf: true,
-      selfMute: false,
-    });
-
-    console.log("🎧 Bot 2 joined voice channel!");
-  } catch (err) {
-    console.error(err);
-  }
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: true,
+    selfMute: false,
+  });
 });
 
-// تسجيل الدخول
 client1.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
